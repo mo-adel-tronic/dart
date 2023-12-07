19d9fe1284e49c8f0a6d9c0a37c4360ad80e1577
mo-adel-tronic <mo.adel.developer@gmail.com>
2023-12-07 09:45:43 +0200

    upload string & number exercises

---
+++ numberProblems.dart
@@ -0,0 +1,60 @@
+/***************
+# Task 1
+
+Create a function that takes a number as input and returns the factorial of the number.
+***************/
+
+// remove this comment and write your code here!!!!
+
+/***************
+# Task 2
+
+Create a function that takes a number as input and check if this number prime or not.
+***************/
+
+// remove this comment and write your code here!!!!
+
+
+/***************
+# Task 3
+
+Create a function that takes a number as input and return string `odd` or `even` after check if this number odd or even.
+***************/
+
+// remove this comment and write your code here!!!!
+
+
+/***************
+# Task 4
+
+Create a function that takes a number as a guess, Generate a random number between 1 and 100. then tell us whether our guessed low, high, or exactly right.
+
+Ex: 
+  input: 20 `my guess`
+  output: low `random 70 > my guess 20`
+***************/
+
+// remove this comment and write your code here!!!!
+
+
+/***************
+# Task 5
+
+Implement a function that takes as input three variables, and returns the largest of the three. 
+
+note: Do this without using the Dart max() function!
+***************/
+
+// remove this comment and write your code here!!!!
+
+void main () {
+  // please test your functions here and use the folowing seprator among calling functions with task number 
+
+  print('''
+
+  #################################
+  Task 1
+  #################################
+  
+  ''');
+}
---
+++ stringProblems.dart
@@ -0,0 +1,68 @@
+/***************
+# Task 1
+
+Create a function that takes a string as input and returns the reversed version of the string.
+***************/
+
+// remove this comment and write your code here!!!!
+
+/***************
+# Task 2
+
+Create a function that takes a string as input and returns the number of vowels (a, e, i, o, u) in the string.
+***************/
+
+// remove this comment and write your code here!!!!
+
+
+/***************
+# Task 3
+
+Create a function that takes a name and age. Print out a message that tells how many years they have to be 100 years old.
+***************/
+
+// remove this comment and write your code here!!!!
+
+
+/***************
+# Task 4
+
+Create a function that takes a long string containing multiple words. Print back to the user the same string, except with the words in backwards order.
+
+Ex: 
+  input: my name is mohamed
+  output: mohamed is name my
+***************/
+
+// remove this comment and write your code here!!!!
+
+
+/***************
+# Task 5
+
+Create a function that takes a square size to draw game boards that look like this:
+
+ --- --- --- 
+|   |   |   | 
+ --- --- ---  
+|   |   |   | 
+ --- --- ---  
+|   |   |   | 
+ --- --- --- 
+
+ note that square size of the above drawing is 3 `that mean 3*3`
+***************/
+
+// remove this comment and write your code here!!!!
+
+void main () {
+  // please test your functions here and use the folowing seprator among calling functions with task number 
+
+  print('''
+
+  #################################
+  Task 1
+  #################################
+  
+  ''');
+}
